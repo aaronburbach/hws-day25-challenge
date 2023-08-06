4401b41ac0478d527d3bd86a9c87661833509770
Aaron Burbach <aaronburbach@cattlekrush.com>
2023-08-06 13:47:47 -0500

    Initial RPS iOS project creation.

---
+++ HWS Day 25 Challenge - RPS/HWS Day 25 Challenge - RPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS Day 25 Challenge - RPS
+//
+//  Created by Aaron Burbach on 8/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS Day 25 Challenge - RPS/HWS Day 25 Challenge - RPS/HWS_Day_25_Challenge___RPSApp.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+@main
+struct HwsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
